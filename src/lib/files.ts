39b43711a7e972617f3cfb25fbe21c301fe6e524
Koen Ongena <koen.ongena@imec.be>
2023-03-01 11:16:49 +0100

    Changing the extensions from PR #3

--- src/lib/files.ts
+++ src/lib/files.ts
@@ -2,5 +2,5 @@ import type {PdfContent} from "./parsers";
 
 const buildBaseFilename = (extension: string) => (pdfContent1: PdfContent) => `${pdfContent1.yearMonth} -Timesheet flexforce - ${pdfContent1.nameOfTheConsultant} (${pdfContent1.companyName}) - signed.${extension}`;
 
-export const buildSignedPdfFileName: (pdfContent: PdfContent) => string = buildBaseFilename('.pdf');
-export const buildSignedExcelFileName: (pdfContent: PdfContent) => string = buildBaseFilename('.xlsx');
+export const buildSignedPdfFileName: (pdfContent: PdfContent) => string = buildBaseFilename('pdf');
+export const buildSignedExcelFileName: (pdfContent: PdfContent) => string = buildBaseFilename('xlsx');
